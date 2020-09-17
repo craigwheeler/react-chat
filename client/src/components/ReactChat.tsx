adb5e6cd1433edf0abc542e7b9d04c774542eea4
Craig Wheeler <craig@craigwheeler.net>
2020-09-17 15:50:37 -0400

    setup qs

--- client/src/components/ReactChat.tsx
+++ client/src/components/ReactChat.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import './ReactChat.css';
+import qs from 'qs';
 
 const ReactChat = () => {
+  // Get username and chatId from URL
+  const { username, chatId } = qs.parse(window.location.search, {
+    ignoreQueryPrefix: true
+  });
+
+  console.log('username: ', username);
+  console.log('chatId: ', chatId);
+
   return (
     <div className="chat-container">
       <main className="chat-main">
