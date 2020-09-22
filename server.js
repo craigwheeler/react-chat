fa787396e559d92c8b83f6d217f7a8fe4cf7fb1a
Craig Wheeler <craig@craigwheeler.net>
2020-09-22 09:37:25 -0400

    refactor server.js

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const path = require("path");
 const http = require("http");
 const express = require("express");
+const moment = require("moment");
 const socketio = require("socket.io");
-const formatMessage = require("./messages");
-const { userJoin, getCurrentUser, userLeave } = require("./users");
-
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
@@ -12,12 +10,41 @@ const io = socketio(server);
 // Set static folder
 app.use(express.static(path.join(__dirname, "client/build")));
 
+const users = [];
 const botName = "Chatbot";
 const welcomeMsg = "Some awesome message about joining. Welcome to the chat!";
 
+// Join chat
+const userJoin = (id, username, chatId) => {
+  const user = { id, username, chatId };
+  users.push(user);
+  return user;
+};
+
+// Get current user
+const getCurrentUser = (id) => {
+  return users.find((user) => user.id === id);
+};
+
+// User leaves chat
+const userLeave = (id) => {
+  const index = users.findIndex((user) => user.id === id);
+  if (index !== -1) {
+    return users.splice(index, 1)[0];
+  }
+};
+
+// Format chat messages
+const formatMessage = (username, text) => {
+  return {
+    username,
+    text,
+    time: moment().format("MMM Do, h:mm:ss a"),
+  };
+};
+
 // Run when client connects
 io.on("connection", (socket) => {
-  // Join Room
   socket.on("joinRoom", ({ username, chatId }) => {
     const user = userJoin(socket.id, username, chatId);
     socket.join(user.chatId);
